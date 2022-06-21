733cf6b613058e662aecb85a6a0395254a04cb39
jinyoung4478 <102174146+jinyoung4478@users.noreply.github.com>
2022-06-22 00:09:26 +0900

    Initialize project

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { theme } from "./theme";
+import GlobalStyle from "./GlobalStyle";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
+
