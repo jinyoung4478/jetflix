99f6bc37fcf2fa2b3fcfa2593d3915e64cb61f32
jinyoung4478 <102174146+jinyoung4478@users.noreply.github.com>
2022-06-29 19:16:33 +0900

    Update

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,3 +1,5 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
@@ -22,6 +24,7 @@ const Banner = styled.div<{ bgPhoto: string }>`
     justify-content: center;
     padding: 60px;
     background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(${(props) => props.bgPhoto});
+    background-position: center;
     background-size: cover;
 `;
 
@@ -35,23 +38,99 @@ const Overview = styled.p`
     width: 50%;
 `;
 
+const Slider = styled.div`
+    position: relative;
+    top: -200px;
+`;
+
+const Row = styled(motion.div)`
+    display: grid;
+    gap: 10px;
+    grid-template-columns: repeat(6, 1fr);
+    margin-bottom: 5px;
+    position: absolute;
+    width: 100%;
+`;
+
+const Box = styled(motion.div) <{ bgPhoto: string }>`
+    background-color: white;
+    background-image: url(${(props) => props.bgPhoto});
+    background-position: center center;
+    background-size: cover;
+    height: 200px;
+    color: red;
+    font-size: 66px;
+`;
+
+const rowVariants = {
+    hidden: {
+        x: window.outerWidth + 10,
+    },
+    visible: {
+        x: 0,
+    },
+    exit: {
+        x: -window.outerWidth - 10,
+    },
+};
+
+const offset = 6;
+
 function Home() {
     const { data, isLoading } = useQuery<IGetMoviesResult>(
         ["movies", "nowPlaying"],
         getMovies,
     );
-    console.log(data)
-    console.log(makeImagePath(data?.results[0].backdrop_path || ""))
+    const [index, setIndex] = useState(0);
+    const [leaving, setLeaving] = useState(false);
+    const increaseIndex = () => {
+        if (data) {
+            if (leaving) return;
+            toggleLeaving();
+            const totalMovies = data?.results.length - 1;
+            const maxIndex = Math.floor(totalMovies / offset) - 1;
+            setIndex((prev) => prev === maxIndex ? 0 : prev + 1);
+        }
+    };
+    const toggleLeaving = () => setLeaving((prev) => !prev)
     return (
         <Wrapper style={{ height: "200vh" }}>
             {isLoading ? (
                 <Loader>Loading ... </Loader>
             ) : (
                 <>
-                    <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+                    <Banner
+                        onClick={increaseIndex}
+                        bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+                    >
                         <Title>{data?.results[0].title}</Title>
                         <Overview>{data?.results[0].overview}</Overview>
                     </Banner>
+                    <Slider>
+                        <AnimatePresence
+                            initial={false}
+                            onExitComplete={toggleLeaving}
+                        >
+                            <Row
+                                variants={rowVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="exit"
+                                transition={{ type: "tween", duration: 1 }}
+                                key={index}
+                            >
+                                {data?.results
+                                    .slice(1)
+                                    .slice(offset * index, offset * index + offset)
+                                    .map((movie) => (
+                                        <Box
+                                            key={movie.id}
+                                            bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                                        />
+                                    ))}
+                            </Row>
+                        </AnimatePresence>
+                    </Slider>
                 </>
             )}
         </Wrapper>
