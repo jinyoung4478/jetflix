da95a0fc468517308c06ddb2fa30ed2b1ee09d1d
jinyoung4478 <102174146+jinyoung4478@users.noreply.github.com>
2022-06-29 17:58:32 +0900

    Update banner

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,6 +1,60 @@
+import { useQuery } from "react-query";
+import styled from "styled-components";
+import { getMovies, IGetMoviesResult } from "../api";
+import { makeImagePath } from "./utils";
+
+const Wrapper = styled.div`
+    background: black;
+`;
+
+const Loader = styled.div`
+    height: 20vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 60px;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(${(props) => props.bgPhoto});
+    background-size: cover;
+`;
+
+const Title = styled.h2`
+    font-size: 2rem;
+    margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+    font-size: 1.0rem;
+    width: 50%;
+`;
+
 function Home() {
+    const { data, isLoading } = useQuery<IGetMoviesResult>(
+        ["movies", "nowPlaying"],
+        getMovies,
+    );
+    console.log(data)
+    console.log(makeImagePath(data?.results[0].backdrop_path || ""))
     return (
-        <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>
+        <Wrapper style={{ height: "200vh" }}>
+            {isLoading ? (
+                <Loader>Loading ... </Loader>
+            ) : (
+                <>
+                    <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+                        <Title>{data?.results[0].title}</Title>
+                        <Overview>{data?.results[0].overview}</Overview>
+                    </Banner>
+                </>
+            )}
+        </Wrapper>
     );
 };
 
