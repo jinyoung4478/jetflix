e00cbd9c26404bc179baa2cb9432920e054f67a3
jinyoung4478 <102174146+jinyoung4478@users.noreply.github.com>
2022-07-05 18:33:27 +0900

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,12 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} ></Route>
-        <Route path="/tv" element={<Tv />} ></Route>
-        <Route path="/search" element={<Search />}></Route>
+        <Route path="/*" element={< Home />}>
+          < Route path="movies/:id" element={< Home />} />
+        </Route>
+        <Route path="movies/:id" element={< Home />} />
+        <Route path="tv" element={<Tv />}></Route>
+        <Route path="search" element={<Search />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "./utils";
+import { useMatch, PathMatch, useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
     background: black;
@@ -16,14 +17,14 @@ const Loader = styled.div`
     align-items: center;
 `;
 
-const Banner = styled.div<{ bgPhoto: string }>`
+const Banner = styled.div<{ bgphoto: string }>`
     height: 100vh;
     width: 100vw;
     display: flex;
     flex-direction: column;
     justify-content: center;
     padding: 60px;
-    background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(${(props) => props.bgPhoto});
+    background-image: linear-gradient(rgba( 0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(${(props) => props.bgphoto});
     background-position: center;
     background-size: cover;
 `;
@@ -52,13 +53,14 @@ const Row = styled(motion.div)`
     width: 100%;
 `;
 
-const Box = styled(motion.div) <{ bgPhoto: string }>`
+const Box = styled(motion.div) <{ bgphoto: string }>`
     background-color: white;
-    background-image: url(${(props) => props.bgPhoto});
+    background-image: url(${(props) => props.bgphoto});
     background-position: center center;
     background-size: cover;
     height: 200px;
     font-size: 66px;
+    cursor: pointer;
     &:first-child{
         transform-origin: center left;
     }
@@ -121,6 +123,8 @@ const infoVariants = {
 const offset = 6;
 
 function Home() {
+    const navigate = useNavigate();
+    const bigMovieMatch: PathMatch<string> | null = useMatch("/movies/:id");
     const { data, isLoading } = useQuery<IGetMoviesResult>(
         ["movies", "nowPlaying"],
         getMovies,
@@ -136,7 +140,11 @@ function Home() {
             setIndex((prev) => prev === maxIndex ? 0 : prev + 1);
         }
     };
-    const toggleLeaving = () => setLeaving((prev) => !prev)
+    const toggleLeaving = () => setLeaving((prev) => !prev);
+    const onBoxClicked = (movieId: number) => {
+        console.log(movieId);
+        navigate(`/movies/${movieId}`);
+    }
     return (
         <Wrapper style={{ height: "200vh" }}>
             {isLoading ? (
@@ -145,7 +153,7 @@ function Home() {
                 <>
                     <Banner
                         onClick={increaseIndex}
-                        bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+                        bgphoto={makeImagePath(data?.results[0].backdrop_path || "")}
                     >
                         <Title>{data?.results[0].title}</Title>
                         <Overview>{data?.results[0].overview}</Overview>
@@ -168,12 +176,14 @@ function Home() {
                                     .slice(offset * index, offset * index + offset)
                                     .map((movie) => (
                                         <Box
+                                            layoutId={movie.id + ""}
                                             key={movie.id}
                                             variants={boxVariants}
+                                            onClick={() => onBoxClicked(movie.id)}
                                             whileHover="hover"
                                             initial="normal"
                                             transition={{ type: "tween", }}
-                                            bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                                            bgphoto={makeImagePath(movie.backdrop_path, "w500")}
                                         >
                                             <Info
                                                 variants={infoVariants}>
@@ -184,6 +194,24 @@ function Home() {
                             </Row>
                         </AnimatePresence>
                     </Slider>
+                    <AnimatePresence>
+                        {bigMovieMatch ?
+                            <motion.div
+                                layoutId={bigMovieMatch.params.id}
+                                style={{
+                                    position: "absolute",
+                                    width: "40vw",
+                                    height: "80vh",
+                                    backgroundColor: "red",
+                                    top: 50,
+                                    left: 0,
+                                    right: 0,
+                                    margin: "0 auto",
+                                }} />
+                            :
+                            null
+                        }
+                    </AnimatePresence>
                 </>
             )}
         </Wrapper>
